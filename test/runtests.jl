be40a6f83143ba52aea1d5a32ca9d22e9e2450a9
shemraz <shem.raz@outlook.com>
2025-09-08 15:47:16 +0100

    ✅ Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using Reflectance
 using Test
 
 @testset "Reflectance.jl" begin
-    # Write your tests here.
+    rl = loaddir("relight-ptm/")    
+    v = (0.0, 1.0)
+    res = light(rl, v)
+    # Dimensions are (p, h, w)
+    coeff_n, coeff_h, coeff_w = size(rl.planes)
+    rgb_n, rgb_h, rgb_w = size(rl.rgb)
+    @test rl.spec.height == coeff_h == rgb_h
+    @test rl.spec.width == coeff_w == rgb_w
+    @test rl.spec.nplanes == coeff_n + rgb_n
+    @test size(res) == (rgb_h, rgb_w)
 end

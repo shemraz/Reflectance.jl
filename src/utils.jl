186f36e8059369e9b88a2a8d9e9e0e8756c47654
shemraz <shem.raz@outlook.com>
2025-09-08 15:40:02 +0100

    🎨 Vectorise dequantisation

--- src/utils.jl
+++ src/utils.jl
@@ -4,10 +4,8 @@
 
 Dequantise an array of coefficients.
 """
-function dequant(planes::Array{Float64,3}, materials::Dict{Symbol, Any})::Array{Float64,3}
-    scale, bias = (materials[:scale], materials[:bias])
-    coeff(c, σ, β) = (c - β) * σ
-    scale3 = reshape(scale, (:, 1, 1))
-    bias3 = reshape(bias, (:, 1, 1))
-    coeff.(planes, scale3, bias3)
+function dequant(planes::Array{Float64,3}, materials::Dict{Symbol,Vector{Float64}})::Array{Float64,3}
+    scale = reshape(materials[:scale], (:, 1, 1))
+    bias = reshape(materials[:bias], (:, 1, 1))
+    return @. (planes - bias) * scale
 end

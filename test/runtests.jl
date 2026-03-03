ff769c33adb01089021c394126a4b3222f734011
shemraz <shem.raz@outlook.com>
2026-03-03 15:26:26 +0000

    ✅ Update tests and add Test dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 using Reflectance
 using Test
 
-@testset "Reflectance.jl" begin
+@testset "reading relight format" begin
     rl = loaddir("relight-ptm/", PTM)    
     v = (0.0, 1.0)
     res = light(rl, v)
-    # Dimensions are (p, h, w)
+    # Dimensions are (n_planes, height, width)
     coeff_n, coeff_h, coeff_w = size(rl.planes)
     rgb_n, rgb_h, rgb_w = size(rl.rgb)
     @test rl.spec.height == coeff_h == rgb_h

edf39c45e38d976d72a45ca9faa4e7ffde3f7fd1
shemraz <shem.raz@outlook.com>
2026-03-10 15:28:40 +0000

    🏷️ Change types from N0f8 to Float64

--- src/types/PTM.jl
+++ src/types/PTM.jl
@@ -1,10 +1,11 @@
-using FixedPointNumbers
-
 struct PTM <: AbstractBasis
-    planes::Array{N0f8,3}
+    model ::Array{Float64,3} # Dequantised array of RGB and coefficient values.
 end
 
-function PTM(planes::AbstractMatrix)::PTM
-    A = cat(planes...; dims = 1)
-
+function PTM(
+    A     ::Array{Float64,3}; # Array of quantised RGB and coefficient values.
+    scale ::Vector{Float64},
+    bias  ::Vector{Float64}
+)::PTM
+    dequantise!(A)
 end
--- src/utils.jl
+++ src/utils.jl
@@ -6,28 +6,24 @@ using Glob
     work on N0f8, which must be between 0.0 and 1.0. Only use N0f8 when rendering
     the output image.
 =#
-function load_plane!(file::String)::Array{N0f8,3}
-    # Load plane into N×H×W array.
+function load_plane!(file::String)::Array{Float64,3}
+    # Load plane into 1×H×W array.
     FileIO.load(file) |> ImageCore.channelview
 end
 
 function read_planes(
     # dir::String
     files::Vector{String} # A list of JPEGs.
- )::Array{N0f8,3}
+ )::Array{Float64,3}
     model = cat(
         load_plane!.(files)...;
         dims=1
     )
 end
 
-"""
-    dequantise(planes::Array{Float64,3}, materials::Dict{Symbol, Any})::Array{Float64,3}
-
-Dequantise an array of coefficients.
-"""
-function dequantise!(A::Array{N0f8,3}, nplanes::Int, scale::Vector{Float64}, bias::Vector{Float64})::Array{N0f8,3}
+function dequantise!(A::Array{Float64,3}, scale::Vector{Float64}, bias::Vector{Float64})::Array{Float64,3}
     # Iterate over planes, where each plane is a height-by-width matrix.
+    nplanes, _, _ = size(A)
     for i ∈ 1:nplanes
         A[i,:,:] *= scale[i] # Multiply 2-D plane by corresponding scalar.
         A[i,:,:] .+= bias[i]

30a47df29753bd2feaa7279da9129ae092efecf1
shemraz <shem.raz@outlook.com>
2026-03-14 22:40:53 +0000

    ✅ Performance tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,16 @@
 using Test
 using Reflectance
 
-@testset "reading relight format" begin
-    rl = Reflectance.load(PTM, "ptm/")
+@testset "Performance" begin
+    @time Reflectance.load(PTM, "ptm/")
+end
+
+@testset "PTM" begin
+    @time rl = Reflectance.load(PTM, "ptm/")
+    @time metadata = Reflectance.load_metadata("ptm/")
     @test rl isa PTM
     @test typeof(rl) <: Basis
+    @test size(rl) == (metadata.nplanes, metadata.height, metadata.width)
 end
+
+

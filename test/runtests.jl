31e3055cbc592d810735ace436cff40854484011
shemraz <shem.raz@outlook.com>
2026-03-15 20:50:27 +0000

    ✅ Restructure tests and add test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,24 @@
 using Test
 using Reflectance
 using Glob
+using BenchmarkTools
+
+data = "data/ptm"
+ptm9 = Reflectance.load(PTM, data)
+metadata = Reflectance.load_metadata(data)
+n_jpegs = length(glob("plane_*.jpg", data))
+
+@testset "Metadata" begin
+    @test metadata.nplanes ==  n_jpegs * 3
+end
+
+@testset "Basis interface" begin
+    @test ptm9 isa PTM
+    @test typeof(ptm9) <: Basis
+    @test size(ptm9) == (metadata.nplanes, metadata.height, metadata.width)
+end
 
 @testset "Performance" begin
-    @showtime Reflectance.load(PTM, "ptm/")
-    A = @showtime Reflectance.load_channels("ptm/plane_0.jpg")
-    m = @showtime Reflectance.load_metadata("ptm/")
-    jpgs = @showtime glob("plane_*.jpg", "ptm/")
-    @showtime Reflectance.getplanes(jpgs)
-    @showtime PTM(A, m)
+    @btime Reflectance.load(PTM, data) samples=5
+    @btime Reflectance.getplanes(glob("plane_*.jpg", data)) samples=5
 end
-
-@testset "PTM" begin
-    @time rl = Reflectance.load(PTM, "ptm/")
-    @time metadata = Reflectance.load_metadata("ptm/")
-    @test rl isa PTM
-    @test typeof(rl) <: Basis
-    @test size(rl) == (metadata.nplanes, metadata.height, metadata.width)
-end
-
-

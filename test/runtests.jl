fcef79ee5af2aceb5ddc323444c87abe5e816d41
shemraz <shemraz@tuta.com>
2026-04-02 16:20:30 +0100

    ✅ Add and restructure tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,23 @@
-using Test
 using Reflectance
-using Glob
-using BenchmarkTools
+using Test
 using ImageCore
-using FileIO
 
-data = "data/ptm"
-ptm9 = relightable(PTM, data)
-metadata = Reflectance.load_metadata(data)
-m_size = (metadata.nplanes, metadata.height, metadata.width)
-n_jpegs = length(glob("plane_*.jpg", data))
+intoindex = Int∘ceil
+downscale(i) = intoindex(1//8 * i)
 
-@testset "Render" begin
-    result = render(ptm9, [0.5,0.5])
-    save("images/result_ptm9.jpg", result)
-end
+@testset "API" begin
+    img, model = loaddir(PTM, "data/ptm"; scale = 1//8)
 
-@testset "Metadata" begin
-    @test metadata.nplanes ==  n_jpegs * 3
-end
+    @testset "Types" begin
+        @test img isa Matrix{RGB{N0f8}}
+        @test model isa PTM
+        @test model.data isa Array{Float64, 3}
+        @test PTM <: AbstractBasis
+    end
 
-@testset "Basis interface" begin
-    @test ptm9 isa PTM
-    @test typeof(ptm9) <: AbstractBasis
-    @test size(ptm9) == (m_size[2:end]...)
-end
-
-@testset "Performance" begin
-    @btime relightable(PTM, data) samples=5
-    @btime Reflectance.getplanes(glob("plane_*.jpg", data), m_size) samples=5
-    @btime Reflectance.load_channels!("$data/plane_0.jpg", Array{Float64,3}(undef, (3, m_size[2], m_size[3])))
-end
-
-@testset "Image" begin
-    img = reinterpret(reshape, RGB{Float64}, ptm9[1:3,:,:])
+    @testset "Methods" begin
+        lvec = (rand(Float64, 2)..., rand(Float64, 1) * -1)
+        T = light(model, lvec)
+        @test T isa Matrix{Float64}
+    end
 end

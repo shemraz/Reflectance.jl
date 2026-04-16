892b3eea6d3837dd73e966afecf555a05f1a09da
shemraz <shemraz@tuta.com>
2026-04-16 15:22:56 +0100

    ♻️ Refactor PTM to better fit the general formulation

--- src/Reflectance.jl
+++ src/Reflectance.jl
@@ -1,19 +1,21 @@
 # Reflectance.jl
 module Reflectance
 
-using Glob, ImageCore, StructTypes
+using Glob, ImageCore, LinearAlgebra, StructTypes
 import JSON3, JpegTurbo
 
 ## Submodules
 ### Core
 include("Core.jl")
 export AbstractBasis
+export dequantise!, dequantize!
 export loaddir
 
 ### Bases
 include("bases/PTM.jl")
 export PTM
-export dequantise!, dequantize!
+include("bases/RBF.jl")
+export RBF
 
 ### Rendering methods
 include("Render.jl")
